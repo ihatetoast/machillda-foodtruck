60b98bce5826f51b9261cb69667be1e703a8cecb
Katy <ihatetoast@gmail.com>
2021-09-13 22:03:27 -0500

    added files i know i'll need.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Header.module.css'
+
+ const Header = (props) => {
+  return (
+    <header>
+      <h1>Machillda's</h1>
+    </header>
+  )
+}
+export default Header
